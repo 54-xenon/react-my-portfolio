cb7d37dfd388d0bc92b69e68156fff035eaf8e31
54-xenon <congas.rabbi_0b@icloud.com>
2023-04-08 18:47:33 +0900

    Homepageの内容を追加

--- src/compornents/Homepage.js
+++ src/compornents/Homepage.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div>
+    <div className='container'>
         <h1>Codeing Journal</h1>
         <p>Codeing Journalです。主にガジェットやプログラミング関連の情報の発信を行っています。</p>
     
