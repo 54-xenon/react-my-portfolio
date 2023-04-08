368c4d2382c17beed4c856e5840e7874c52dc8eb
54-xenon <congas.rabbi_0b@icloud.com>
2023-04-08 18:59:26 +0900

    Homapageのレイアウトを変更

--- src/compornents/Homepage.js
+++ src/compornents/Homepage.js
@@ -5,7 +5,8 @@ const Homepage = () => {
     <div className='container'>
         <h1>Codeing Journal</h1>
         <p>Codeing Journalです。主にガジェットやプログラミング関連の情報の発信を行っています。</p>
-    
+
+      
     </div>
   )
 }
