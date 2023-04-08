d22359a0af95e8e6c5508bfc5e968a740b1ceaeb
54-xenon <congas.rabbi_0b@icloud.com>
2023-04-08 16:04:31 +0900

    reactのひな形完成

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>Apple信者@RAM不足</h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Blog</a>
+                </li>
+                <li>
+                    <a href="#">Link</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
